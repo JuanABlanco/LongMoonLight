ef3eda12c7893fe787463632420fd783b6bb1c32
Juan Blanco <juan15597l@gmail.com>
2021-08-21 01:53:14 +0200

    Funcionalidad de giro

---
+++ Assets/Scripts/SpinObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinObject : MonoBehaviour
+{
+    //El objeto necesita un collider
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            transform.Rotate(Vector3.forward, 20f);
+        } else if (Input.GetMouseButtonDown(0))
+        {
+            transform.Rotate(Vector3.forward, -20f);
+        }
+    }
+}
